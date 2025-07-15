881272b5a0d0bff7169df675a4e4f05ff9897a4b
martute25 <englishwithanm@gmail.com>
2025-07-15 00:27:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
+const ramos = [
+  {
+    nombre: "Lengua y Escritura Académica",
+    id: "lengua-academica",
+    requisitos: []
+  },
+  {
+    nombre: "Psicología Educacional",
+    id: "psico-educacional",
+    requisitos: []
+  },
+  {
+    nombre: "Pedagogía",
+    id: "pedagogia",
+    requisitos: []
+  },
+  {
+    nombre: "Didáctica General",
+    id: "didactica-general",
+    requisitos: []
+  },
+  {
+    nombre: "Educación Digital",
+    id: "educacion-digital",
+    requisitos: []
+  },
+  {
+    nombre: "Lengua Inglesa e Interculturalidad I",
+    id: "interculturalidad-1",
+    requisitos: []
+  },
+  {
+    nombre: "Fonética, Fonología y Dicción Inglesa I",
+    id: "fonetica-1",
+    requisitos: []
+  },
+  {
+    nombre: "Gramática Inglesa I",
+    id: "gramatica-1",
+    requisitos: []
+  },
+  {
+    nombre: "Práctica Profesional Docente I",
+    id: "ppd-1",
+    requisitos: []
+  },
+  {
+    nombre: "Historia y Política de la Educación Argentina",
+    id: "historia-arg",
+    requisitos: ["pedagogia"]
+  },
+  {
+    nombre: "Educación Sexual Integral",
+    id: "esi",
+    requisitos: []
+  },
+  {
+    nombre: "Sujetos del Aprendizaje",
+    id: "sujetos-aprendizaje",
+    requisitos: ["psico-educacional", "interculturalidad-1", "fonetica-1", "gramatica-1"]
+  },
+  // Podés seguir agregando el resto con el mismo formato...
+];
 
